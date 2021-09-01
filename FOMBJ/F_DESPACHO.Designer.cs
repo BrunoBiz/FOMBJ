8624ee8aee7ddc6d760c147209b3eb0b9efff66a
jalmeidas <49208546+jalmeidas@users.noreply.github.com>
2021-09-01 18:30:39 -0300

    TELA DESPACHO

--- FOMBJ/F_DESPACHO.Designer.cs
+++ FOMBJ/F_DESPACHO.Designer.cs
@@ -28,79 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(F_DESPACHO));
             this.panel3 = new System.Windows.Forms.Panel();
-            this.label9 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
-            this.lblDespachoMercadoria = new System.Windows.Forms.Label();
             this.sqlSelectCommand1 = new System.Data.SqlClient.SqlCommand();
             this.sqlInsertCommand1 = new System.Data.SqlClient.SqlCommand();
             this.sqlUpdateCommand1 = new System.Data.SqlClient.SqlCommand();
             this.sqlDeleteCommand1 = new System.Data.SqlClient.SqlCommand();
             this.sqlDataAdapter1 = new System.Data.SqlClient.SqlDataAdapter();
+            this.button3 = new System.Windows.Forms.Button();
+            this.TxtCodbarras = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.TxtOrigem = new System.Windows.Forms.TextBox();
+            this.TxtDestino = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.panel3.Controls.Add(this.label9);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.TxtDestino);
+            this.panel3.Controls.Add(this.TxtOrigem);
+            this.panel3.Controls.Add(this.dataGridView1);
+            this.panel3.Controls.Add(this.TxtCodbarras);
             this.panel3.Controls.Add(this.button3);
-            this.panel3.Controls.Add(this.pictureBox3);
-            this.panel3.Controls.Add(this.label11);
             this.panel3.Controls.Add(this.label12);
-            this.panel3.Location = new System.Drawing.Point(13, 90);
+            this.panel3.Location = new System.Drawing.Point(3, 3);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(772, 428);
+            this.panel3.Size = new System.Drawing.Size(796, 502);
             this.panel3.TabIndex = 0;
             this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
-            // 
-            // label9
-            // 
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label9.Location = new System.Drawing.Point(10, 204);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(174, 62);
-            this.label9.TabIndex = 2;
-            this.label9.Text = "Está mercadoria pode ser Despachada pelo Preço abaixo!";
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.RoyalBlue;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(87, 288);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(107, 26);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "Despachar";
-            this.button3.UseVisualStyleBackColor = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(4, 48);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(188, 144);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 1;
-            this.pictureBox3.TabStop = false;
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.ForeColor = System.Drawing.Color.Green;
-            this.label11.Location = new System.Drawing.Point(7, 287);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(80, 25);
-            this.label11.TabIndex = 1;
-            this.label11.Text = "R$650";
             // 
             // label12
             // 
@@ -108,23 +71,10 @@
             this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label12.Location = new System.Drawing.Point(8, 15);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(183, 25);
+            this.label12.Size = new System.Drawing.Size(274, 25);
             this.label12.TabIndex = 1;
-            this.label12.Text = "MERCADORIA 3";
-            // 
-            // lblDespachoMercadoria
-            // 
-            this.lblDespachoMercadoria.AutoSize = true;
-            this.lblDespachoMercadoria.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.lblDespachoMercadoria.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblDespachoMercadoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F);
-            this.lblDespachoMercadoria.ForeColor = System.Drawing.Color.AliceBlue;
-            this.lblDespachoMercadoria.Location = new System.Drawing.Point(0, 0);
-            this.lblDespachoMercadoria.Name = "lblDespachoMercadoria";
-            this.lblDespachoMercadoria.Size = new System.Drawing.Size(813, 78);
-            this.lblDespachoMercadoria.TabIndex = 1;
-            this.lblDespachoMercadoria.Text = "Despacho de Mercadorias\r\n";
-            this.lblDespachoMercadoria.Click += new System.EventHandler(this.lblDespachoMercadoria_Click);
+            this.label12.Text = "Insira o código de barras\r\n";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
             // 
             // sqlDataAdapter1
             // 
@@ -133,11 +83,85 @@
             this.sqlDataAdapter1.SelectCommand = this.sqlSelectCommand1;
             this.sqlDataAdapter1.UpdateCommand = this.sqlUpdateCommand1;
             // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(655, 410);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 65);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Despachar";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // TxtCodbarras
+            // 
+            this.TxtCodbarras.Location = new System.Drawing.Point(288, 20);
+            this.TxtCodbarras.Name = "TxtCodbarras";
+            this.TxtCodbarras.Size = new System.Drawing.Size(492, 20);
+            this.TxtCodbarras.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(288, 58);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(492, 289);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // TxtOrigem
+            // 
+            this.TxtOrigem.Location = new System.Drawing.Point(13, 118);
+            this.TxtOrigem.Name = "TxtOrigem";
+            this.TxtOrigem.Size = new System.Drawing.Size(183, 20);
+            this.TxtOrigem.TabIndex = 5;
+            this.TxtOrigem.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // TxtDestino
+            // 
+            this.TxtDestino.Location = new System.Drawing.Point(13, 178);
+            this.TxtDestino.Name = "TxtDestino";
+            this.TxtDestino.Size = new System.Drawing.Size(183, 20);
+            this.TxtDestino.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(498, 410);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 65);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Cancelar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "ORIGEM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "DESTINO";
+            // 
             // F_DESPACHO
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.lblDespachoMercadoria);
             this.Controls.Add(this.panel3);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "F_DESPACHO";
@@ -145,24 +169,26 @@
             this.Load += new System.EventHandler(this.MySecondCustmControl_Load);
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label lblDespachoMercadoria;
         private System.Data.SqlClient.SqlCommand sqlSelectCommand1;
         private System.Data.SqlClient.SqlCommand sqlInsertCommand1;
         private System.Data.SqlClient.SqlCommand sqlUpdateCommand1;
         private System.Data.SqlClient.SqlCommand sqlDeleteCommand1;
         private System.Data.SqlClient.SqlDataAdapter sqlDataAdapter1;
+        private System.Windows.Forms.TextBox TxtCodbarras;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox TxtDestino;
+        private System.Windows.Forms.TextBox TxtOrigem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
